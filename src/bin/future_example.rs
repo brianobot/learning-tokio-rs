67d01e16b01f7ed1bef1a123fc43fd1a3eb65529
Brian Obot <brianobot@gmail.com>
2026-05-28 13:12:40 +0100

    Async in Depth

--- src/bin/future_example.rs
+++ src/bin/future_example.rs
@@ -3,19 +3,28 @@ use std::pin::Pin;
 use std::task::{Context, Poll};
 use std::time::{Duration, Instant};
 
-use tokio::time::MissedTickBehavior::Delay;
-
 
 struct Delay {
     when: Instant
 }
 
+impl Future for Delay {
+    type Output = &'static str;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if Instant::now() >= self.when {
+            println!("Hello World");
+            
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
     let when = Instant::now() + Duration::from_secs(2);
     let future = Delay { when };
 
-    
+    let out = future.await;
+    assert!(out, "done");
 }
 
