9d33d9ff37c7ea43cafb863e80cb4538de37f374
Brian Obot <brianobot@gmail.com>
2026-05-28 18:28:30 +0100

    Async in Depth

--- src/bin/bliss.rs
+++ src/bin/bliss.rs
@@ -1,6 +1,18 @@
+use std::{thread, time::{Duration, Instant}};
+
 
 
 #[tokio::main]
 async fn main() {
+    let now = Instant::now();
     
+    perform_some_action().await;
+    perform_some_action().await;
+    perform_some_action().await;
+    perform_some_action().await;
+}
+
+
+async fn perform_some_action() {
+    thread::sleep(Duration::from_secs(2));
 }
