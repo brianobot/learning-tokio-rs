e4f02ecf76d9817a3a4bb67e0f4c8c5f118085c1
Brian Obot <brianobot@gmail.com>
2026-05-28 09:37:21 +0100

    Messaging Passing in Tokio

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,9 @@ async fn main() {
     // the forwarders are the senders, the processing unit is the receiver
     // 
     // also notice that the channel capacity is set to 32, if messages are not processed as quickly as 
-    // they are received, once it reaches 32, calling send().await would 
+    // they are received, once it reaches 32, calling send().await would go to sleep until the channel is fred up
     let (tx, rx) = mpsc::channel::<Command>(32);
+
+    // sending from multiple tasks is done by cloning the sender
+    let tx2 = 
 }
