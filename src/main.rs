4346b1e8be06739c10a528b55dc38d81ca90486c
Brian Obot <brianobot@gmail.com>
2026-05-28 09:36:17 +0100

    Messaging Passing in Tokio

--- src/main.rs
+++ src/main.rs
@@ -20,5 +20,8 @@ async fn main() {
     // in this approach, we can create a task to manage central processing like a connection
     // and the forward all the messages (data) needed to be processeed to that task
     // the forwarders are the senders, the processing unit is the receiver
+    // 
+    // also notice that the channel capacity is set to 32, if messages are not processed as quickly as 
+    // they are received, once it reaches 32, calling send().await would 
     let (tx, rx) = mpsc::channel::<Command>(32);
 }
