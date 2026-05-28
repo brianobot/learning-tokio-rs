d65db499e6cdbcd99e4894b62f4b522c97770e86
Brian Obot <brianobot@gmail.com>
2026-05-28 16:11:30 +0100

    Async in Depth

--- src/bin/mini_tokio.rs
+++ src/bin/mini_tokio.rs
@@ -68,14 +68,9 @@ impl MiniTokio {
         self.tasks.push_back(Box::pin(future));
     }
 
-    fn run(&mut self) {
-        let waker = task::noop_waker();
-        let mut cx = Context::from_waker(&waker);
-
-        while let Some(mut task) = self.tasks.pop_front() {
-            if task.as_mut().poll(&mut cx).is_pending() {
-                self.tasks.push_back(task);
-            }
+    fn run(&self) {
+        while let Ok(task) = self.scheduled.recv() {
+            task.poll();
         }
     }
 }
