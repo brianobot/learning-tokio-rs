5a956ab0a81da2f27161a4ae522800a7a14d7a4b
Brian Obot <brianobot@gmail.com>
2026-05-28 09:40:34 +0100

    Messaging Passing in Tokio

--- src/main.rs
+++ src/main.rs
@@ -23,17 +23,25 @@ async fn main() {
     // 
     // also notice that the channel capacity is set to 32, if messages are not processed as quickly as 
     // they are received, once it reaches 32, calling send().await would go to sleep until the channel is fred up
-    let (tx, rx) = mpsc::channel(32);
+    let (tx, mut rx) = mpsc::channel(32);
 
     // sending from multiple tasks is done by cloning the sender
     let tx2 = tx.clone();
+    let tx3 = tx.clone();
 
     tokio::spawn(async move {
+        println!("Sending Message A");
         tx.send("A").await.unwrap();
     });
 
     tokio::spawn(async move {
+        println!("Sending Message B");
         tx2.send("B").await.unwrap();
+    });
+
+    tokio::spawn(async move {
+        println!("Sending Message C");
+        tx3.send("C").await.unwrap();
     });
 
     while let Some(message) = rx.recv().await {
