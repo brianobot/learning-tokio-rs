70ad8f81bada10f561a113a96c023e46563ddaeb
Brian Obot <brianobot@gmail.com>
2026-06-03 12:25:50 +0100

    [Async in Depth](Async: What is blocking?)

--- src/bin/when_to_block.rs
+++ src/bin/when_to_block.rs
@@ -1,10 +1,13 @@
+use std::time::Duration;
+
 
 
 #[tokio::main]
 async fn main() {
-    let blocking_handle = tokio::task::spawn_blocking(async move {
-        
+    let blocking_handle = tokio::task::spawn_blocking(|| {
+        println!("Inside Spawn blocking");
+        std::thread::sleep(Duration::from_secs(10));
     });
 
-    block
+    blocking_handle.await.unwrap()
 }
